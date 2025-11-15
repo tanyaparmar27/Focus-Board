fd2286751ee84ca764c834e600ea620f9a909b22
Tanya Parmar <tanyaparmar03@gmail.com>
2025-11-15 16:42:17 +0530

    deployment on netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/Focus-Board/',
+export default defineConfig({
+  base: '/',
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
